1bd0004fbaa6b63d4caf77a48a07e4f6bee5b299
playoffthecuff <jmobile@bk.ru>
2025-03-02 18:22:51 +0300

    fix: revalidate

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
       );
     }
 
-    revalidatePath("/");
+    revalidatePath("/", "layout");
 
     const message = "Content updated. Контент обновлён.";
     const title = "Check the site. Проверьте сайт.";
