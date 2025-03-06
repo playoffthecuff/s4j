9a62be52584e2df40b3e1f60670139d9631331f1
playoffthecuff <jmobile@bk.ru>
2025-03-06 05:19:12 +0300

    fix: remove comp util

--- lib/utils/isRubicon.ts
+++
@@ -1,7 +0,0 @@
-import { EventType } from "../../app/[lang]/(with-footer)/events/fetchEvents";
-
-export const DELTA = 3600 * 24;
-
-export const isRubicon = (current: EventType, prev?: EventType) =>
-  Date.now() - new Date(current.date).getTime() > DELTA &&
-  new Date(prev?.date ?? 0).getTime() - Date.now() > -DELTA;
